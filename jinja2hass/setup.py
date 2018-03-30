8c31593bc0660ed073f580c86bc5c7d4f20ee213
Avraham David Gelbish <adg@adgelb.fish>
2018-03-29 20:00:48 -0400

    first commit

---
+++ jinja2hass/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+  name='jinja2hass',
+  version='0.1',
+  author='Avraham David Gelbfish',
+  author_email='adg@adgelb.fish',
+  install_requires=[
+          "jinja2==2.10"
+      ],
+  scripts=['jinja2hass']
+)
